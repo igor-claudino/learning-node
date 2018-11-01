ff4fb6ca165f8cd2414b2e76a9f7bf793e34e390
Igor Claudino de França Costa <igor.claudino.ic@gmail.com>
2018-11-01 14:41:36 -0300

    Fim aula 2

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,7 @@
 const express = require('express');
-
 const User = require('../models/user');
+const utils =require('../config/utils');
+const authMiddleware = require('../middleware/auth');
 
 const router = express.Router();
 
@@ -13,17 +14,25 @@ router.post('/', async (req, res) => {
         const user = await User.create(req.body);
 
         user.password = undefined;
-        return res.send({ user });
+
+        return res.send({
+            user,
+            token: utils.generateToken(
+                { id: user.id }
+            )
+        });
 
     } catch (err) {
         return res.status(400).send({ error: 'Registration failed' });
     }
 });
 
+router.use(authMiddleware);
+
 router.get('/', async (req, res) => {
     try {
         const users = await User.find();
-        return res.send({ users });
+        return res.send({ users, userId:req.userId });
     } catch (err) {
         return res.status(400).send({ error: 'Get failed' });
     }
--- src/index.js
+++ src/index.js
@@ -8,5 +8,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
 require('./controllers/user.controller')(app);
+require('./controllers/login.controller')(app);
 
 app.listen('3000');
